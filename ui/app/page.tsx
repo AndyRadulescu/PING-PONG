4a883a9be0e7039f6b58df8a949f629fb0a270ba
Andy Radulescu <radulescu.eduard.andrei@gmail.com>
2023-08-13 12:50:13 +0200

    stylised main page

--- ui/app/page.tsx
+++ ui/app/page.tsx
@@ -16,8 +16,16 @@ export default async function Home() {
 
   return (
     <main>
-      <p>{JSON.stringify(data)}</p>
-      <Link href="/game"> New Game</Link>
+      <div class="container mx-auto px-4 h-screen flex flex-col">
+        <div class="grow flex justify-center items-center">
+          <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 border border-green-700 rounded">
+            <Link href="/game">Start New Game</Link>
+          </button>
+        </div>
+        <div class="grow-0 items-end flex justify-center">
+          <p>version: {JSON.stringify(data.version)}</p>
+        </div>
+      </div>
     </main>
   );
 }
