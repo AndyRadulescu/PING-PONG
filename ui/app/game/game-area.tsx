0d6100101cc3a793e885a697e0bb30aa5e34a39e
Andy Radulescu <radulescu.eduard.andrei@gmail.com>
2023-11-18 20:53:33 +0100

    add ball image and display it on the canvas

--- ui/app/game/game-area.tsx
+++ ui/app/game/game-area.tsx
@@ -1,5 +1,29 @@
+import { useEffect, useRef } from 'react';
+
 const GameArea = () => {
-  return <canvas id="gameArea" width="1000px" height="600px" className="border border-amber-50 rounded-md"></canvas>;
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current!;
+    const ctx = canvas.getContext('2d')!;
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    const ball = new Image();
+    ball.src = '/ball.png';
+    ball.onload = () => {
+      ctx.drawImage(ball, 10, 10, 50, 50);
+    };
+
+  }, []);
+
+  return <canvas
+    id="gameArea"
+    width="1000px"
+    height="600px"
+    className="border border-amber-50 rounded-md"
+    ref={canvasRef}
+  />;
 };
 
 export default GameArea;
