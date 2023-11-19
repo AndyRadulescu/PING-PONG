9cedc576d12217a2ab20c95194dbb88d62552187
Andy Radulescu <radulescu.eduard.andrei@gmail.com>
2023-11-19 16:01:23 +0100

    add rackets and positioned ball in the middle

--- ui/app/game/game-area.tsx
+++ ui/app/game/game-area.tsx
@@ -1,20 +1,37 @@
 import { useEffect, useRef } from 'react';
-import { BALL_DIAMETER, GAME_AREA_HEIGHT, GAME_AREA_WIDTH } from '@/app/game/game-config';
+import { BALL_DIAMETER, GAME_AREA_HEIGHT, GAME_AREA_WIDTH, RACKET_HEIGHT, RACKET_WIDTH } from '@/app/game/game-config';
 
 const GameArea = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const drawThisPlayer = (ctx: CanvasRenderingContext2D) => {
+    ctx.beginPath();
+    ctx.fillRect(375, GAME_AREA_HEIGHT - RACKET_HEIGHT, RACKET_WIDTH, RACKET_HEIGHT);
+    ctx.stroke();
+  };
+
+  const drawOpponentPlayer = (ctx: CanvasRenderingContext2D) => {
+    ctx.beginPath();
+    ctx.fillRect(375, 0, RACKET_WIDTH, RACKET_HEIGHT);
+    ctx.stroke();
+  };
+
+  const drawBall = (ctx: CanvasRenderingContext2D) => {
+    const ball = new Image();
+    ball.src = '/ball.svg';
+    ball.onload = () => {
+      ctx.drawImage(ball, GAME_AREA_WIDTH / 2 - BALL_DIAMETER / 2, GAME_AREA_HEIGHT / 2 - BALL_DIAMETER / 2, BALL_DIAMETER, BALL_DIAMETER);
+    };
+  };
 
   useEffect(() => {
     const canvas = canvasRef.current!;
     canvas.width = GAME_AREA_WIDTH;
     canvas.height = GAME_AREA_HEIGHT;
     const ctx = canvas.getContext('2d')!;
-
-    const ball = new Image();
-    ball.src = '/ball.svg';
-    ball.onload = () => {
-      ctx.drawImage(ball, 0, 0, BALL_DIAMETER, BALL_DIAMETER);
-    };
+    drawThisPlayer(ctx);
+    drawOpponentPlayer(ctx);
+    drawBall(ctx);
   }, []);
 
   return <canvas
--- ui/app/game/game-config.ts
+++ ui/app/game/game-config.ts
@@ -2,5 +2,7 @@ export const GAME_AREA_WIDTH = 1000;
 export const GAME_AREA_HEIGHT = 600;
 
 export const BALL_DIAMETER = 30;
+export const RACKET_WIDTH = 250;
+export const RACKET_HEIGHT = 25;
 export const MAX_BALL_RANGE_X = GAME_AREA_WIDTH - BALL_DIAMETER;
 export const MAX_BALL_RANGE_Y = GAME_AREA_HEIGHT - BALL_DIAMETER;
