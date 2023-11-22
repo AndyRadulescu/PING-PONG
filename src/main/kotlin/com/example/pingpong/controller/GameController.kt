e28af667896f63f676ab10e700f1a9864fba75ec
Andy Radulescu <radulescu.eduard.andrei@gmail.com>
2023-11-22 16:56:57 +0100

    implement scheduler

--- src/main/kotlin/com/example/pingpong/controller/GameController.kt
+++ src/main/kotlin/com/example/pingpong/controller/GameController.kt
@@ -2,24 +2,25 @@ package com.example.pingpong.controller
 
 import com.example.pingpong.config.TaskDefinitionBean
 import com.example.pingpong.config.TaskSchedulingService
-import com.example.pingpong.data.TaskDefinition
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.messaging.handler.annotation.DestinationVariable
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.handler.annotation.SendTo
 import org.springframework.messaging.simp.SimpMessagingTemplate
+import org.springframework.scheduling.TaskScheduler
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import java.util.UUID
+import java.time.Duration
+import java.util.*
 
 
 @Controller
 class GameController {
 
-//    @Autowired
-//    private lateinit var template: SimpMessagingTemplate
+    @Autowired
+    private lateinit var template: SimpMessagingTemplate
 
     @Autowired
     private lateinit var taskSchedulingService: TaskSchedulingService
@@ -27,9 +28,25 @@ class GameController {
     @Autowired
     private lateinit var taskDefinitionBean: TaskDefinitionBean
 
+    @Autowired
+    private lateinit var om: ObjectMapper
+
+    @Autowired
+    private lateinit var scheduler: TaskScheduler
+
     @MessageMapping("/msg/{name}")
     @SendTo("/topic/{name}")
-    fun receiveMessage(/*@DestinationVariable name: String,*/ message: String): String {
+    fun receiveMessage(@DestinationVariable name: String, message: String): String {
+        println(name)
+        this.scheduler.scheduleAtFixedRate(
+            {
+                run {
+                    println("send message")
+                    this.template.convertAndSend("/topic/$name", om.writeValueAsString("Hello"))
+                }
+            },
+            Duration.ofMillis(20)
+        )
         return message
     }
 
@@ -46,11 +63,4 @@ class GameController {
             taskDefinitionBean,
         )
     }
-
-//    @Scheduled(fixedRate = 5000)
-//    fun scheduleResponse() {
-//        Thread.sleep(1000) // simulated delay
-//        println("scheduled")
-//        this.template.convertAndSend("/topic/CR51", "Hello");
-//    }
 }
