973abb924ea9f671e72003a89d3622cf2fa456ca
Andy Radulescu <radulescu.eduard.andrei@gmail.com>
2023-11-25 18:01:20 +0100

    add store

--- ui/app/core/model/game-message.ts
+++ ui/app/core/model/game-message.ts
@@ -1,4 +1,15 @@
-export interface BallLocation {
-  x:string;
-  y:string;
+export interface BallVector {
+  x: string;
+  y: string;
+}
+
+export interface PlayerRacket {
+  x: number;
+}
+
+export interface GameState {
+  roomId: String;
+  ballVector: BallVector;
+  player1: PlayerRacket;
+  player2: PlayerRacket;
 }
