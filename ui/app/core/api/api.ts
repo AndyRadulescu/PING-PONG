9d53286e517988eb496b62d4d9cf2bfd3ef1d585
Andy Radulescu <radulescu.eduard.andrei@gmail.com>
2023-11-28 20:22:35 +0100

    add api layer

---
+++ ui/app/core/api/api.ts
@@ -0,0 +1,29 @@
+const CLIENT_URL = 'http://localhost:8080/api';
+export const getData = async (): Promise<{ version: number }> => {
+  const res = await fetch(CLIENT_URL);
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  return await res.json() as { version: number };
+};
+
+export const startGame = async (roomId: String): Promise<string> => {
+  const res = await fetch(`${CLIENT_URL}/addTask/${roomId}`);
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  return await res.json() as string;
+};
+
+export const stopGame = async (taskId: String): Promise<Record<'canceled', boolean>> => {
+  const res = await fetch(`${CLIENT_URL}/cancelTask/${taskId}`);
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  return await res.json() as Record<'canceled', boolean>;
+};
--- ui/app/page.tsx
+++ ui/app/page.tsx
@@ -1,15 +1,6 @@
 import React from 'react';
 import NewGame from '@/app/shared/new-game';
-
-async function getData(): Promise<{ version: number }> {
-  const res = await fetch('http://localhost:8080/api');
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data');
-  }
-
-  return res.json() as unknown as { version: number };
-}
+import { getData } from '@/app/core/api/api';
 
 export default async function Home() {
   const data = await getData();
