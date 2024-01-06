a78be6fec8cfc19cddd7e6f53e55a01b205b7a01
Andy Radulescu <radulescu.eduard.andrei@gmail.com>
2024-01-06 18:14:59 +0100

    restrict rackets to go out of the game area

--- ui/app/core/engine/racket-engine.ts
+++ ui/app/core/engine/racket-engine.ts
@@ -1,13 +1,28 @@
 import { GameState, ThisPlayer } from '@/app/core/model/game.model';
 import { Client } from 'stompjs';
-import { RACKET_SPEED } from '@/app/game/game-config';
+import { GAME_AREA_WIDTH, RACKET_SPEED, RACKET_WIDTH } from '@/app/game/game-config';
 
 export const moveRacket = (moveRacket: MoveRacket) => {
   const { actualPlayer, gameState, updateGameState, stompClient, roomId, direction } = moveRacket;
-  let newPosition = direction === Direction.LEFT ? gameState[actualPlayer].x - RACKET_SPEED : gameState[actualPlayer].x + RACKET_SPEED;
+  let newPosition = calculatePosition({ direction, actualPlayer, gameState });
   const updatedPlayer = { [actualPlayer]: { x: newPosition } };
   updateGameState(updatedPlayer);
   stompClient.send(`/app/msg/${roomId}`, {}, JSON.stringify(updatedPlayer));
+};
+
+const calculatePosition = ({ direction, actualPlayer, gameState }: {
+  direction: Direction,
+  actualPlayer: ThisPlayer,
+  gameState: GameState
+}) => {
+  switch (direction) {
+    case Direction.LEFT: {
+      return gameState[actualPlayer].x >= 0 ? gameState[actualPlayer].x - RACKET_SPEED : gameState[actualPlayer].x;
+    }
+    case Direction.RIGHT: {
+      return gameState[actualPlayer].x <= GAME_AREA_WIDTH - RACKET_WIDTH ? gameState[actualPlayer].x + RACKET_SPEED : gameState[actualPlayer].x;
+    }
+  }
 };
 
 export type MoveRacket = {
