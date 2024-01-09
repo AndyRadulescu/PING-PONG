d6550dd4ee24ad01d9dc9499e14cdfcddd7ce1fc
Andy Radulescu <radulescu.eduard.andrei@gmail.com>
2024-01-09 20:54:56 +0100

    bounce and get angle off the racket

--- src/main/kotlin/com/example/pingpong/BallPhysics.kt
+++ src/main/kotlin/com/example/pingpong/BallPhysics.kt
@@ -1,13 +1,11 @@
 package com.example.pingpong
 
-import com.example.pingpong.data.BALL_DIAMETER
-import com.example.pingpong.data.GAME_AREA_HEIGHT
-import com.example.pingpong.data.GameState
-import com.example.pingpong.data.RACKET_HEIGHT
+import com.example.pingpong.data.*
 
 fun calculateNextBallPosition(gameState: GameState): Unit {
     bounceRacketWithAnglePlayer1(gameState)
     gameState.ballVector.y += gameState.ballVector.velocityY
+    gameState.ballVector.x += gameState.ballVector.velocityX
     endGame(gameState)
 }
 
@@ -22,9 +20,43 @@ fun endGame(gameState: GameState) {
 fun bounceWallRight() {}
 fun bounceWallLeft() {}
 fun bounceRacketWithAnglePlayer1(gameState: GameState) {
-    if (gameState.ballVector.y >= GAME_AREA_HEIGHT - BALL_DIAMETER - RACKET_HEIGHT
-        && (gameState.ballVector.x >= gameState.player1.x && gameState.player1.x <= gameState.player1.x + RACKET_HEIGHT)
+    val ballX = gameState.ballVector.x
+    val ballY = gameState.ballVector.y
+    val ballDistanceOnRacket1 = ballOnRacketAngle(ballX, gameState.player1.x)
+    val ballDistanceOnRacket2 = ballOnRacketAngle(ballX, gameState.player2.x)
+
+    if (ballY >= GAME_AREA_HEIGHT - BALL_DIAMETER - RACKET_HEIGHT
+        && ballDistanceOnRacket1 != -1
     ) {
-        gameState.ballVector.velocityY = -10;
+        gameState.ballVector.velocityY = -10
+        gameState.ballVector.velocityX = ballDistanceOnRacket1
     }
+
+    if (ballY <= RACKET_HEIGHT
+        && ballDistanceOnRacket2 != -1
+    ) {
+        gameState.ballVector.velocityY = +10
+        gameState.ballVector.velocityX = ballDistanceOnRacket1
+    }
+}
+
+fun ballOnRacketAngle(ballX: Int, racketX: Int): Int {
+    val distanceOnRacket = ballX - racketX
+    println(distanceOnRacket)
+    if (distanceOnRacket in 0..50) {
+        return 30
+    }
+    if (distanceOnRacket in 50..100) {
+        return 10
+    }
+    if (distanceOnRacket in 100..150) {
+        return 0
+    }
+    if (distanceOnRacket in 150..200) {
+        return 10
+    }
+    if (distanceOnRacket in 200..250) {
+        return 30
+    }
+    return -1
 }
